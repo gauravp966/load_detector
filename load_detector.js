5360b6ccbf2af0599812001ced42c881455b51bc
Gaurav Patil <gauravp966@gmail.com>
2017-07-18 16:56:03 -0400

    Add files via upload

---
+++ load_detector.js
@@ -0,0 +1,8 @@
+
+function load_detector(callback){
+
+    window.onload = function() {    //window.onload waits for the whole page to load properly.
+    callback;                       //the callback function is called back.
+    };
+
+}
