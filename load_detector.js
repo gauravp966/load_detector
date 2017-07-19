bd79e3485931b4e9ea948b6dba90b27fc7a04b7c
Gaurav Patil <gauravp966@gmail.com>
2017-07-19 17:09:11 -0400

    Update load_detector.js

--- load_detector.js
+++ load_detector.js
@@ -1,22 +1,34 @@
-var root = 'https://jsonplaceholder.typicode.com';
-
-$.ajax({								//I have used a dummy REST service only to trigger the 'ajaxStop' event,
-  url: root + '/posts/1',						// even if there are no ajax requests in the website.
-  method: 'GET'
-}).then(function(data) {
-  console.log(data);
-});
+//Using PerformanceResourceTiming API
 
 function load_detector(callback){
 
-	$(window).on("load", function(){				//I used window onload to check the DOM.
+	if ( !('performance' in window) ||
+	       !('getEntriesByType' in window.performance) ||
+	       !(window.performance.getEntriesByType('resource') instanceof Array)		//to check if the API is supported by the browser.
+	     ) {
+	     console.log("No API support");
+	  	} 
+	  	else {
 
-		$(window).ajaxStop(function(){				//'ajaxstop' is triggered when the Ajax requests are complete and there are no other outstanding requests.
-											
-			callback.call();				//the callback function is called back.
+			window.addEventListener('load', function() {				//waits till the DOM is loaded.
 
-		});
+				var resource = performance.getEntriesByType("resource");	//grabs ALL of the present resources in the website.
 
-	});
-	
+				for (var i=0; i < resource.length; i++) {			//iteration through all resources.
+
+					t = resource[i].responseEnd;				//the 'responseEnd' property returns a timestamp immediately after the browser receives the last byte of the resource..
+												//or immediately before the transport connection is closed, whichever comes first.
+				
+												//we wait in this loop till all the resources are iterated and 'responeEnd' timestamp is received.
+				}
+
+				callback.call();						//once, all the resources have responded, we call the callback function.
+
+				console.log("responseEnd: "+ t);				//just to display the responseEnd timestamp on the console.
+
+				
+
+			});
+		}
+
 }
