5f7760f31bfb1f3b273882bb4c0dd2e6ba5ae048
Gaurav Patil <gauravp966@gmail.com>
2017-07-18 23:34:38 -0400

    Update load_detector.js

--- load_detector.js
+++ load_detector.js
@@ -1,7 +1,7 @@
 var root = 'https://jsonplaceholder.typicode.com';
 
-$.ajax({								//I have used a dummy REST service only to trigger the 'ajaxStop' event, even if there are no ajax requests in the website.
-  url: root + '/posts/1',								
+$.ajax({								//I have used a dummy REST service only to trigger the 'ajaxStop' event,
+  url: root + '/posts/1',						// even if there are no ajax requests in the website.
   method: 'GET'
 }).then(function(data) {
   console.log(data);
@@ -9,14 +9,14 @@ $.ajax({								//I have used a dummy REST service only to trigger the 'ajaxStop
 
 function load_detector(callback){
 
-	$(window).ajaxStop(function() {					//'ajaxstop' is triggered when the Ajax requests are complete and there are no other outstanding requests.
+	$(window).on("load", function(){				//I used window onload to check the DOM.
 
-		$(window).on("load",function(){				//I used window onload just in case if the Ajax request completes before the DOM is fully loaded.
-
-			callback.call();				//the callback function is called back
+		$(window).ajaxStop(function(){				//'ajaxstop' is triggered when the Ajax requests are complete and there are no other outstanding requests.
+											
+			callback.call();				//the callback function is called back.
 
 		});
 
 	});
-
+	
 }
