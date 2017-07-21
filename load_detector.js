a09e9398889afa48d8028fa87570b4d9573b1355
Gaurav Patil <gauravp966@gmail.com>
2017-07-21 11:56:39 -0400

    Update load_detector.js

--- load_detector.js
+++ load_detector.js
@@ -1,4 +1,4 @@
-//Using ResourceTiming API
+/PerformanceResourceTiming API
 
 function load_detector(callback){
 
@@ -7,28 +7,28 @@ function load_detector(callback){
 	       !(window.performance.getEntriesByType('resource') instanceof Array)		//to check if the API is supported by the browser.
 	     ) {
 	     console.log("No API support");
-	  	} 
-	  	else {
+	} 
+	else {
 
-			window.addEventListener('load', function() {				//waits till the DOM is loaded.
+		window.addEventListener('load', function() {							//waits till the DOM is loaded.
 
-				var resource = performance.getEntriesByType("resource");	//grabs ALL of the present resources in the website.
+			var resource = performance.getEntriesByType("resource");				//grabs ALL of the present resources in the website.
 
-				for (var i=0; i < resource.length; i++) {			//iteration through all resources.
+			for (var i=0; i < resource.length; i++) {							//iteration through all resources.
 
-					t = resource[i].responseEnd;				//the 'responseEnd' property returns a timestamp immediately after the browser receives the last byte of the resource..
-												//or immediately before the transport connection is closed, whichever comes first.
-				
-												//we wait in this loop till all the resources are iterated and 'responeEnd' timestamp is received.
+				while(resource[i].responseEnd == null || resource[i].responseEnd == undefined){		//here we wait until we get a responseEnd.
+					if(resource[i].responseEnd != undefined){					//or immediately before the transport connection is closed, whichever comes first.
+						
+						break;
+						
+					}
 				}
-
-				callback.call();						//once, all the resources have responded, we call the callback function.
-
-				console.log("responseEnd: "+ t);				//just to display the responseEnd timestamp on the console.
-
-				
-
-			});
-		}
-
+			
+				console.log("responseEnd: "+ resource[i].responseEnd + ". resources: " + resource);	//shoes the resources and its end time via responseEnd.											
+			}
+		 																				
+			callback.call();										//once, all the resources have responded, we call the callback function.																			
+										
+		});
+	}
 }
